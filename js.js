e9e7bbe536fd4a6f50509f2636ab846f5ad3fdb5
dvikster <dvikster@gmail.com>
2017-05-19 11:09:03 +0300

    added files

---
+++ js.js
@@ -0,0 +1,21 @@
+var vushAlfabet=[
+    {
+        'id': 'A',
+        'imgSrc': 'images/big_sinica.jpg'
+    },
+    {
+        'id': 'Б',
+        'imgSrc': 'images/chizh.jpeg'
+    },
+    {
+        'id': 'В',
+        'imgSrc': 'images/galka.jpg',
+    },
+    {
+        'id': 'Г',
+        'imgSrc': 'images/korostel.jpg',
+     }
+]
+
+
+var name = document.getElementById('name');
